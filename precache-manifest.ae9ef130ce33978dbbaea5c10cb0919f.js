e05af8d6b45d7e5eee7380631ca80d39b5ba03d5
kkowalczykk <kovalczyk97@gmail.com>
2020-07-01 23:58:34 +0200

    Updates

---
+++ precache-manifest.ae9ef130ce33978dbbaea5c10cb0919f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d967801cc17b8c0db34dfa5c69baa7b7",
+    "url": "/Sorting-Visualizer/index.html"
+  },
+  {
+    "revision": "5964b62ccb17fe073480",
+    "url": "/Sorting-Visualizer/static/css/main.72e1c30c.chunk.css"
+  },
+  {
+    "revision": "8823e2bb5a7a8e337c65",
+    "url": "/Sorting-Visualizer/static/js/2.5e34a9e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Visualizer/static/js/2.5e34a9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5964b62ccb17fe073480",
+    "url": "/Sorting-Visualizer/static/js/main.4831d615.chunk.js"
+  },
+  {
+    "revision": "fff1963fb4d00de61fc3",
+    "url": "/Sorting-Visualizer/static/js/runtime-main.66cf885b.js"
+  }
+]);
